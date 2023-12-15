8829928f8679bebde29dd782b801ab5af472040f
jcarlson <jake.carlson90@gmail.com>
2023-12-15 13:16:48 -0800

    routes controllers and seed file

---
+++ config/database.js
@@ -0,0 +1,8 @@
+require('dontenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.mongo_URI)
+
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo')
+})
